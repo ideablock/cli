29039bc251118902c9cbe7e206b882bf53957cbb
Eli Sheets <eli@ideablock.io>
2019-06-30 22:48:33 -0400

    prompts there, exec partial

--- index.js
+++ index.js
@@ -14,9 +14,9 @@ _  /    __  /  __  /
 const shell = require('shelljs')
 const inquirer = require('inquirer')
 const path = require('path')
+const zipper = require('zip-local')
 const crypto = require('crypto') //encryption
 const fs = require('fs') //this might not be needed, I think node made this to ship w/ latest LTS but will check
-const JSZip = require('jszip')
 const async = require('async')
 
 //Create IdeaBlock Directory
@@ -39,65 +39,88 @@ let ideaJSON = {}
 
 const questions = [{
   type: 'input',
-  name:'title',
+  name: 'tags',
+  message: 'Idea Title?'
+},
+{
+  type: 'input',
+  name: 'title',
   message: 'Idea Title?'
 },
 {
   //Description
   type: 'input',
-  name:'description',
+  name: 'description',
   message: 'Additional Description?'
 },
+//Parent Idea(s) - need to finish this after getting details on endpoint/return from Adam
 {
-  //File selection (maybe)
-  type: 'input',
-  name:'title',
-  message: 'Idea Title?'
+  type: 'checkbox',
+  name: 'parent',
+  message: 'Parent Idea(s)? (You can select multiple parent ideas)'
 },
+//Thumbnail
 {
-  type: 'input',
-  name:'title',
-  message: 'Idea Title?'
+  type: 'checkbox',
+  name: 'thumb',
+  message: 'Please select a thumbnail file from the list of idea files (or select none for default thumbnail)',
+  choices: [
+    //list files in .idea as separate choices (those with .png, .jpg, .jpeg mime types)
+  ]
 },
+//Tags
 {
   type: 'input',
-  name:'title',
-  message: 'Idea Title?'
+  name: 'tags',
+  message: 'Please enter any tags you would like to add to the idea (comma- or semicolon-separated list)'
 },
+//Public or Private
 {
-  type: 'input',
-  name:'title',
-  message: 'Idea Title?'
-},
-{
-  type: 'input',
-  name:'title',
-  message: 'Idea Title?'
+  type: 'checkbox',
+  message: 'Would you like this idea to be public or private?',
+  name: 'publication',
+  choices: [
+    {
+      name: 'Public',
+      checked: true
+    },
+    {
+      name: 'Private'
+    }
+  ]
 }]
 
 //Subroutines
 
-//copy all files in directory
-function copyFiles {
-
+//copy all files into .idea directory
+function copyFiles(callback) {
+  fs.readdir(__dirname, function (err, files) {
+    if (err) {
+      return console.log('Unable to read the files in the present directory --> ' + __dirname)
+    }
+    files.forEach(function (file) {
+      fs.copyFile(path.join(__dirname, file), path.join(__dirname, '.idea', file), (err) => {
+        console.log(path.join(__dirname, '.idea', file) + 'written to .idea dir')
+      })
+    })
+  })
 }
 
 //Zip array of files
-function ideaZip (files) {
-
-  return ideaFile
+function ideaZip(callback) {
+  let date = new Date()
+  let ideaFileName = 'IdeaFile-' + date + '.zip'
+  zipper.sync.zip('./.idea/').compress().save(ideaFileName)
+  console.log(ideaFileName + ' written')
+  callback(ideaFileName)
 }
 
-//Encrypt Zip
-function ideaFileEncrypt (ideaFile) {
+function hashFile(callback) {
 
-...
-
-return ideaFile
 }
 
 //Create idea.txt for title and description
-function ideaText (title, description) {
+function ideaText(title, description) {
   ...
 
   return ideaTextFile
@@ -105,10 +128,10 @@ function ideaText (title, description) {
 
 
 inquirer.prompt(questions)
-.then(answers => {
-  ideaFile = ideaZip(answers.files)
-  async.series([login, questions, ideaText, ])
-  console.log(answers)
-})
+  .then(answers => {
+    ideaFile = ideaZip(answers.files)
+    async.series([login, questions, ideaText,])
+    console.log(answers)
+  })
 
 
